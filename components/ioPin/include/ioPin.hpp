18cd5e7f66d49633b96308da59718d087dcb9645
Julius Schneider <j.schneider@schtrom.com>
2023-09-08 12:07:06 +0200

    ioPin: add analog functionalities

--- components/ioPin/include/ioPin.hpp
+++ components/ioPin/include/ioPin.hpp
@@ -26,14 +26,16 @@ namespace gardener
         virtual g_err get(uint8_t &value) { return G_ERR_NO_IMPLEMENTATION; }
         virtual bool canSet() { return false; }
         virtual bool canGet() { return false; }
+        virtual g_err setVoltage(int32_t voltage_mV) { return G_ERR_NO_IMPLEMENTATION; }
+        virtual g_err getVoltage(int32_t &voltage_mV) { return G_ERR_NO_IMPLEMENTATION; }
         static ioPin *dummyPin();
 
     protected:
-        uint8_t _pinNumber;        
+        uint8_t _pinNumber;
         bool _connected;
 
     private:
-        static ioPin * _dummyPin;
+        static ioPin *_dummyPin;
     };
 
     class ioPinDummy : public ioPin
@@ -48,10 +50,10 @@ namespace gardener
         bool canGet();
 
     protected:
-        uint8_t _pinNumber;        
+        uint8_t _pinNumber;
 
     private:
-        static ioPin * _dummyPin;
+        static ioPin *_dummyPin;
     };
 
 } // namespace gardener
