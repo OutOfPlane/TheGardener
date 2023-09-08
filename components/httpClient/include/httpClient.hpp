8eba35babb44d8754d2012725a61b3235fd18426
Julius Schneider <j.schneider@schtrom.com>
2023-09-09 01:43:09 +0200

    ADD: httpClient

---
+++ components/httpClient/include/httpClient.hpp
@@ -0,0 +1,25 @@
+#ifndef G_HTTP_CLIENT_H
+#define G_HTTP_CLIENT_H
+
+#include "gardenObject.hpp"
+#include "esp_http_client.h"
+
+namespace gardener
+{
+    class httpClient : public gardenObject
+    {
+    public:
+        httpClient(const char *name);
+        virtual ~httpClient() {}
+        g_err get(char *responseBuffer, uint16_t responseBufferSize, const char *requestUrl);
+        g_err getPrintf(char *responseBuffer, uint16_t responseBufferSize, const char *requestUrl, ...);
+
+    protected:
+    private:
+        esp_http_client_config_t _config;
+        esp_http_client_handle_t _client;
+    };
+
+} // namespace gardener
+
+#endif
---
+++ components/httpClient/src/httpClient.cpp
@@ -0,0 +1,130 @@
+#include "httpClient.hpp"
+#include "esp_netif.h"
+#include "esp_crt_bundle.h"
+
+const char *cert = R"EOF(
+-----BEGIN CERTIFICATE-----
+MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw
+TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
+cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4
+WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu
+ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY
+MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc
+h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+
+0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U
+A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW
+T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH
+B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC
+B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv
+KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn
+OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn
+jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw
+qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI
+rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV
+HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq
+hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL
+ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ
+3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK
+NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5
+ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur
+TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC
+jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc
+oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq
+4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA
+mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d
+emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
+-----END CERTIFICATE-----)EOF";
+
+using namespace gardener;
+
+gardener::httpClient::httpClient(const char *name)
+    : gardenObject(name)
+{
+    memset(&_config, 0, sizeof(_config));
+    _config.url = "http://www.google.de";
+    // _config.crt_bundle_attach = esp_crt_bundle_attach;
+    _config.cert_pem = cert;
+    _client = esp_http_client_init(&_config);
+}
+
+g_err gardener::httpClient::get(char *responseBuffer, uint16_t responseBufferSize, const char *requestUrl)
+{
+    int content_length = 0;
+    esp_http_client_set_url(_client, requestUrl);
+    esp_http_client_set_method(_client, HTTP_METHOD_GET);
+
+    esp_err_t err = esp_http_client_open(_client, 0);
+    if (err != ESP_OK)
+    {
+        G_LOGE("Failed to open HTTP connection: %s", esp_err_to_name(err));
+    }
+    else
+    {
+        content_length = esp_http_client_fetch_headers(_client);
+        if (content_length < 0)
+        {
+            G_LOGE("HTTP client fetch headers failed");
+        }
+        else
+        {
+            int data_read = esp_http_client_read_response(_client, responseBuffer, responseBufferSize);
+            if (data_read >= 0)
+            {
+                responseBuffer[data_read] = 0;
+                G_LOGI("HTTP GET Status = %d, content_length = %d",
+                       esp_http_client_get_status_code(_client),
+                       esp_http_client_get_content_length(_client));
+            }
+            else
+            {
+                responseBuffer[0] = 0;
+                G_LOGE("Failed to read response");
+            }
+        }
+    }
+    esp_http_client_close(_client);
+
+    return G_OK;
+}
+
+const char *hexChars = "0123456789ABCDEF";
+
+g_err gardener::httpClient::getPrintf(char *responseBuffer, uint16_t responseBufferSize, const char *requestUrl, ...)
+{
+    char tmp[256];
+    va_list args;
+    va_start(args, requestUrl);
+    vsprintf(tmp, requestUrl, args);
+    va_end(args);
+    // urlencode the getPARAMs
+    char tmpENC[256];
+    char *tmpPtr = tmp;
+    char *tmpENCPtr = tmpENC;
+    uint8_t paramStart = 0;
+    while (*tmpPtr)
+    {
+        *tmpENCPtr = *tmpPtr;
+        if (paramStart)
+        {
+            if(*tmpPtr == '=')
+                *tmpENCPtr = *tmpPtr;
+            else if(*tmpPtr == '&')
+                *tmpENCPtr = *tmpPtr;
+            else if(isalnum(*tmpPtr) == 0)
+            {
+                *tmpENCPtr = '%';
+                tmpENCPtr++;
+                *tmpENCPtr = hexChars[((*tmpPtr) >> 4) & 0xF];
+                tmpENCPtr++;
+                *tmpENCPtr = hexChars[((*tmpPtr) >> 0) & 0xF];
+            }
+        }
+        if (*tmpPtr == '?')
+            paramStart = 1;
+        tmpPtr++;
+        tmpENCPtr++;
+    }
+    *tmpENCPtr = '\0';
+    G_LOGI("Executing request: %s", tmpENC);
+    return get(responseBuffer, responseBufferSize, tmpENC);
+}
